561dc8196afe09b2731706914e9d926746eea28e
Salvador AZ <salvadoracienz@gmail.com>
2022-08-01 13:02:47 +0200

    Commit previo a empezar a crear nuevas entidades, hasta ahora todo funciona.

--- src/sensores/sensor.entity.ts
+++ src/sensores/sensor.entity.ts
@@ -3,6 +3,7 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class Sensor {
   //Aqui falta gestionar como identificamos a cada sensor u_90x_xxxxxx ?¿
+  //Cambiar decorador al que permite definir mi clave
   @PrimaryGeneratedColumn() 
   id: number;
 
